eba9151106032e71386cad7748de3a7dc994281d
Borland04 <McGarret160497@gmail.com>
2023-12-02 12:16:18 +0300

    Puzzle 2.2

--- app/Two/App.hs
+++ app/Two/App.hs
@@ -15,6 +15,8 @@ playCubesGame constraints filename = do
   let games = map parseGame content
   let res = foldr (+) 0 . map gameId . filter (isGamePossible constraints) $ games
   putStrLn $ "Task 1: " ++ show res
+  let res2 = foldr (+) 0 . map power $ games
+  putStrLn $ "Task 2: " ++ show res2
 
 parseGame :: String -> Game
 parseGame s = readSets . dropColon . readGameId . dropGame $ (s, Game)
@@ -39,9 +41,19 @@ parseGame s = readSets . dropColon . readGameId . dropGame $ (s, Game)
         readCube s = let [num, color] = splitOn " " $ strip s
                      in (unpack color, read (unpack num) :: Int)
 
+-- Task 1
 isGamePossible :: CubeSet -> Game -> Bool
 isGamePossible constraints (Game _ cubesets) = all (\set -> all (notViolatesConstraint constraints) set) cubesets
   where notViolatesConstraint :: CubeSet -> RevealedCube -> Bool
         notViolatesConstraint constr (color, num) = case find ((== color) . fst) constr of
                                                       Just (_, maxNum) -> num <= maxNum
                                                       Nothing -> False
+
+-- Task 2
+power :: Game -> Int
+power (Game _ cubeset) = foldr (*) 1 . map snd . foldr accumulateCubes [] $ concat cubeset
+  where accumulateCubes :: RevealedCube -> CubeSet -> CubeSet
+        accumulateCubes c [] = [c]
+        accumulateCubes c@(color, num) (acc:accs)
+          | fst acc == color = (color, max num (snd acc)) : accs
+          | otherwise = acc : accumulateCubes c accs
