3671278b210f729d586981894fb28322c4e21fc4
Borland04 <McGarret160497@gmail.com>
2023-12-03 11:44:34 +0300

    Puzzle 3.2

--- app/Three/App.hs
+++ app/Three/App.hs
@@ -14,6 +14,9 @@ readEngineSchematics = do
   putStrLn "Task 1: "
   let res = foldr (+) 0 . map number $ findNumsAdjacentToSymbols schematics
   putStrLn $ show res
+  putStrLn "Task 2: "
+  let res2 = foldr (+) 0 . map (\(a, b) -> number a * number b) $ findGears schematics
+  putStrLn $ show res2
 
 parseSchematics :: [String] -> [SchematicsAtom]
 parseSchematics = parseSchematics' 0
@@ -30,13 +33,22 @@ parseSchematics = parseSchematics' 0
           | otherwise = Symbol [ch] row currentIndex : parseLine row (currentIndex + 1) chs
 
 
+-- Task 1
 findNumsAdjacentToSymbols :: [SchematicsAtom] -> [SchematicsAtom]
 findNumsAdjacentToSymbols schem = let syms = filter (\a -> case a of Symbol _ _ _ -> True; _ -> False) schem
                                       nums = filter (\a -> case a of PartNumber _ _ _ _ -> True; _ -> False) schem
                                   in filter (isAdjacentToSymbol' syms) nums
   where isAdjacentToSymbol' :: [SchematicsAtom] -> SchematicsAtom -> Bool
         isAdjacentToSymbol' syms num = any (flip isAdjacent $ num) syms
-        isAdjacent :: SchematicsAtom -> SchematicsAtom -> Bool
-        isAdjacent (Symbol _ symRow symIdx) (PartNumber _ numRow startIdx endIdx) = (symRow >= numRow - 1 && symRow <= numRow + 1) && (symIdx >= startIdx - 1 && symIdx <= endIdx + 1)
+
+isAdjacent :: SchematicsAtom -> SchematicsAtom -> Bool
+isAdjacent (Symbol _ symRow symIdx) (PartNumber _ numRow startIdx endIdx) = (symRow >= numRow - 1 && symRow <= numRow + 1) && (symIdx >= startIdx - 1 && symIdx <= endIdx + 1)
 
 
+-- Task 2
+findGears :: [SchematicsAtom] -> [(SchematicsAtom, SchematicsAtom)]
+findGears schem = let gearSyms = filter (\a -> case a of Symbol "*" _ _ -> True; _ -> False) $ schem
+                      nums = filter (\a -> case a of PartNumber _ _ _ _ -> True; _ -> False) schem
+                  in map (\[a, b] -> (a, b)) . filter (\gearNums -> length gearNums == 2) . map (findAdjucentNums nums) $ gearSyms
+  where findAdjucentNums :: [SchematicsAtom] -> SchematicsAtom -> [SchematicsAtom]
+        findAdjucentNums nums gear = filter (isAdjacent gear) nums
