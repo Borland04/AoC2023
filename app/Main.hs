f7c8ba92c0dc837d7bf918f79ec895eb36bb980a
Borland04 <McGarret160497@gmail.com>
2023-12-06 12:01:03 +0300

    Add 05 and 06 to main method

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,8 @@ import One.App (readCalibrations)
 import Two.App (playCubesGame)
 import Three.App (readEngineSchematics)
 import Four.App (scratchcards)
+import Five.App (seeds)
+import Six.App (boatsRace)
 
 
 main :: IO ()
@@ -19,3 +21,10 @@ main = do
   putStrLn ""
   putStrLn "-- Day 4 --"
   scratchcards
+  putStrLn ""
+  putStrLn "-- Day 5 --"
+  seeds
+  putStrLn ""
+  putStrLn "-- Day 6 --"
+  boatsRace
+
