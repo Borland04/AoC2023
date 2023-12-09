a63014021d4d3017e1ab70c5a16bd7b7ccfbc72a
Borland04 <McGarret160497@gmail.com>
2023-12-09 11:12:56 +0300

    Puzzle 9.2

--- app/Nine/App.hs
+++ app/Nine/App.hs
@@ -3,11 +3,18 @@ module Nine.App where
 ecosystemReport :: IO ()
 ecosystemReport = do
   readings <- map (map (read :: String -> Int) . words) . lines <$> readFile "app/Nine/input.txt"
-  let res = foldr (+) 0 . map extrapolate . map diffSequences $ readings
+  let diffs = map diffSequences readings
+  let res = foldr (+) 0 . map extrapolate $ diffs 
   putStrLn $ "# Task 1: " ++ show res
+  let res2 = foldr (+) 0 . map extrapolateBackwards $ diffs 
+  putStrLn $ "# Task 2: " ++ show res2
+
 
 extrapolate :: [[Int]] -> Int
 extrapolate = foldr (+) 0 . map last
+
+extrapolateBackwards :: [[Int]] -> Int
+extrapolateBackwards = foldr (-) 0 . map head
 
 diffSequences :: [Int] -> [[Int]]
 diffSequences nums = let next = process nums
